be5588fcf7ef1b419194429bf52b94c98925524e
HaifaaJanoudi <138940955+HaifaaJanoudi@users.noreply.github.com>
2025-01-25 16:56:45 -0500

    sidebar implemented

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import EmergencyLandingPage from './assets/EmergencyLandingPage.jsx'
+import Sidebar from './assets/Sidebar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
-    < EmergencyLandingPage />
+    <Sidebar />
+    {/* < EmergencyLandingPage /> */}
     
   </StrictMode>,
 )
