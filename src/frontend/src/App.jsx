f3994b475c23a4b6661a6a819bc82a6d0304baef
Hawa-Afnane Said <hawa21244@gmail.com>
2025-01-25 21:07:39 -0500

    fixed the App.jsx issue

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LogIn from './components/LogIn/LogIn';
+import SignUpPage from './components/SignUp/SignUp';
+import './App.css';
+
+function App() {
+  return (  
+    <BrowserRouter>
+      <div className='app-container'>
+        <Routes>
+          <Route path="/" element={<LogIn />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LogIn />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
